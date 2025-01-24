dc38a029592574c4b3495008bf86ab94728a8c6c
shivbhagat03 <109543410+shivbhagat03@users.noreply.github.com>
2025-01-24 16:26:16 +0530

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# InfluxDB Configuration
+
 INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
 if not INFLUXDB_TOKEN:
     raise ValueError("INFLUXDB_TOKEN environment variable is not set")
@@ -15,7 +15,6 @@ INFLUX_CONFIG = {
     'url': "http://localhost:8086"
 }
 
-# Fields configuration
 FIELDS = [
     "T1", "T2", "T3", "T4", "T5",
     "RH_1", "RH_2", "RH_3", "RH_4", "RH_5",
