c477755807a4dfd0a4cf40db271fd1652d5b2e7b
shivbhagat03 <109543410+shivbhagat03@users.noreply.github.com>
2025-01-24 16:26:45 +0530

    app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import dash #type: ignore
 from layout import create_layout
 from callbacks import register_callbacks
 
-# Initialize the Dash app
 app = dash.Dash(__name__)
 
-# Create the layout
 create_layout(app)
 
-# Register callbacks
+
 register_callbacks(app)
 
 if __name__ == '__main__':
